951eca3bbb27d235b9d6d2c77a646f7c80489185
RaresIgescu <raresigescu@gmail.com>
2024-11-13 13:24:58 +0200

    Adaugare rute totale pentru fiecare pagina

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render("index.ejs");
+})
+
+app.get('/posts', (req, res) => {
+    res.render("posts.ejs")
+});
+
+app.get('/quotes', (req, res) => {
+    res.render("quotes.ejs");
+})
+
+app.get('/miscelanous', (req, res) => {
+    res.render("miscelanous.ejs");
 })
 
 app.listen(port, () => {
