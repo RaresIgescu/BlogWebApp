e2e6b64914a06c4897f4a8e582f1f26ae6186b47
RaresIgescu <raresigescu@gmail.com>
2024-11-14 19:34:47 +0200

    Adaugare functionalitate de adaugare postare

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
+const posts = [];
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -10,16 +13,29 @@ app.get('/', (req, res) => {
 })
 
 app.get('/posts', (req, res) => {
-    res.render("posts.ejs")
+    res.render("posts.ejs", { posts });
 });
 
 app.get('/quotes', (req, res) => {
     res.render("quotes.ejs");
-})
+});
 
 app.get('/miscelanous', (req, res) => {
     res.render("miscelanous.ejs");
-})
+});
+
+app.get('/adaugare', (req, res) => {
+    res.render("adaugare.ejs");
+});
+
+app.post('/submit', (req, res) => {
+    const newPost ={
+        title: req.body.title,
+        descriere: req.body.descriere
+    };
+    posts.push(newPost);
+    res.redirect('/posts');
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
